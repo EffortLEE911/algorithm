0e186099e4efeb4897dabafe81e0bb0f17348827
LazyLEE1008 <shlee6630@gmail.com>
2024-09-27 00:10:09 +0900

    5강

--- lecture5/lecture5/lecture5/프로그래머스_게임맵최단거리.cpp
+++ lecture5/lecture5/lecture5/프로그래머스_게임맵최단거리.cpp
@@ -4,19 +4,27 @@
 using namespace std;
 
 int visited[101][101];
-vector<vector<int>>maps = { {1, 0, 1, 1, 1} ,{1, 0, 1, 0, 1},{1, 0, 1, 1, 1},{1, 1, 1, 0, 1},{0, 0, 0, 0, 1} };
 
-//vector<vector<int>>maps = { {1, 1, 1, 1, 1} ,
-//							{0, 0, 1, 0, 1},
-//							{1, 1, 1, 0, 1},
-//							{1, 0, 0, 0, 1},
-//							{1, 1, 1, 1, 1} }; // 깊이 우선 탐색이 최단거리가 안되는 반례
 
-void dfs(int y, int x);
+void dfs(int y, int x, vector<vector<int>>maps);
 
 int main() {
+
+	//vector<vector<int>>maps = { {1, 0, 1, 1, 1} ,{1, 0, 1, 0, 1},{1, 0, 1, 1, 1},{1, 1, 1, 0, 1},{0, 0, 0, 0, 1} };
+
+	//vector<vector<int>>maps = { {1, 1, 1, 1, 1} ,
+	//							{0, 0, 1, 0, 1},
+	//							{1, 1, 1, 0, 1},
+	//							{1, 0, 0, 0, 1},
+	//							{1, 1, 1, 1, 1} }; // 깊이 우선 탐색이 최단거리가 안되는 반례
+
+	vector<vector<int>>maps = { {1, 0, 1, 1, 1},
+								{1, 0, 1, 0, 1},
+								{1, 0, 1, 1, 1},
+								{1, 1, 1, 0, 0},
+								{0, 0, 0, 0, 1}};
 		
-	int answer = 0;
+	int answer = -1;
 	//for (int i = 0; i < maps.size(); i++) {
 	//	for (int j = 0; j < maps[0].size(); j++) {
 	//		cout << maps[i][j] << " ";
@@ -27,27 +35,29 @@ int main() {
 
 	int y_len = maps.size();
 	int x_len = maps[0].size();
+	visited[y_len - 1][x_len - 1] = -1;
 
-	if (maps[y_len - 2][x_len - 1] == 0 && maps[y_len - 1][x_len - 2] == 0) {
-		answer = -1;
-	}
-	else {
 
-		dfs(0, 0);
+	dfs(0, 0, maps);
 
-		answer = visited[y_len - 1][x_len - 1];
+	answer = visited[y_len - 1][x_len - 1];
 
-	}
+	
 	cout << answer << endl;
 	
 
 	
-
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			cout << visited[i][j] << "  ";
+		}
+		cout << endl;
+	}
 
 
 }
 
-void dfs(int y, int x) {
+void dfs(int y, int x, vector<vector<int>>maps) {
 
 	int d_y[4] = { 1, 0, -1, 0 };
 	int d_x[4] = { 0, 1, 0, -1 };
@@ -65,10 +75,10 @@ void dfs(int y, int x) {
 
 
 		if ((next_y >= 0 && next_y<maps.size()) && (next_x >= 0 && next_x<maps.size())) {
-			if (maps[next_y][next_x] == 1 && visited[next_y][next_x] == 0)
+			if (maps[next_y][next_x] == 1 && (visited[next_y][next_x] == 0 || visited[next_y][next_x] == -1))
 			{
 				visited[next_y][next_x] = visited[y][x] + 1;
-				dfs(next_y, next_x);
+				dfs(next_y, next_x, maps);
 			}
 		}
 
