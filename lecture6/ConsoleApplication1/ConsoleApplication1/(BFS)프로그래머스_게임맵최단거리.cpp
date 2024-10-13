74767f201308badc7fd2264208d3f9d73a40044a
LazyLEE1008 <shlee6630@gmail.com>
2024-10-14 03:15:27 +0900

    fix

--- lecture6/ConsoleApplication1/ConsoleApplication1/(BFS)프로그래머스_게임맵최단거리.cpp
+++ lecture6/ConsoleApplication1/ConsoleApplication1/(BFS)프로그래머스_게임맵최단거리.cpp
@@ -15,15 +15,19 @@ int main()
                                 {1, 0, 1, 1, 1},
                                 {1, 1, 1, 0, 1},
                                 {0, 0, 0, 0, 1} };
+
+
+    int answer = 0;
                 // 아래 오른쪽 위 왼쪽
     int d_y[4] = { 1, 0, -1, 0 };   //이동을 의미한다 y가 증가하면 배열은 아래로,즉 위로 이동
     int d_x[4] = { 0, 1, 0, -1 };   // x가 증가하면 배열은 오른쪽으로이동 , 감소는 그 반대
 
+
+    
     int y_len = maps.size()-1;
     int x_len = maps[0].size()-1;
 
     visited[0][0] = 1;
-    visited[y_len][x_len] = -1;
 
     queue<pair<int, int>> q;
     q.push(make_pair(0, 0));
@@ -45,9 +49,9 @@ int main()
             next_y = cur_y + d_y[i];
             next_x = cur_x + d_x[i];
 
-            if ((next_y >= 0 && next_y <= y_len) && (next_x >= 0 && next_x <= x_len))
+            if ((next_y >= 0 && next_y <= y_len) && (next_x >= 0 && next_x <= x_len)) 
             {
-                if (maps[next_y][next_x] == 1 && (visited[next_y][next_x] == 0|| visited[next_y][next_x] == -1))
+                if (maps[next_y][next_x] == 1 && (visited[next_y][next_x] == 0))
                 {
                     q.push(make_pair(next_y, next_x));
                     visited[next_y][next_x] = visited[cur_y][cur_x] + 1;
@@ -56,6 +60,15 @@ int main()
 
         }
     }
+
+    if (visited[y_len][x_len] == 0) {
+        answer = -1;
+    }
+    else {
+        answer = visited[y_len][x_len];
+    }
+
+    return answer;
 
 
 
