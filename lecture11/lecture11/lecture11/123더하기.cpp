a06be5371e174069c8273943a862e164a57f0ddf
LazyLEE1008 <shlee6630@gmail.com>
2024-11-22 07:31:03 +0900

    lecture11

--- lecture11/lecture11/lecture11/123더하기.cpp
+++ lecture11/lecture11/lecture11/123더하기.cpp
@@ -1,46 +1,30 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
-int arr[5001];
+int arr[12];
 
-int main() {
-	int N = 18;
 
-	int answer = 0;
+int main()
+{
 
-	arr[1] = -1;
-	arr[2] = -1;
-	arr[3] = 1;
-	arr[4] = -1;
-	arr[5] = 1;
-	arr[6] = 2;
-	arr[7] = -1;
-	arr[8] = 2;
-	arr[9] = 3;
-	arr[10] = 2;
-	arr[11] = 3;
-	arr[12] = 4;
+	int T = 3;
+	int n = 0;
 
-	int a = 0;
-	int b = 0;
-	for (int i = 13; i < 5001; i++) {
-		a = arr[i - 3];
-		b = arr[i - 5];
+	arr[1] = 1;
+	arr[2] = 2;
+	arr[3] = 4;
 
-		if (a > b) {
-			arr[i] = b + 1;
-		}
-		else {
-			arr[i] = a + 1;
-		}
-
+	for (int i = 4; i < 12; i++) {
+		arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
 	}
-	cin >> N;
 
-	cout << arr[N] << endl;
+	cin >> T;
 
-
+	for (int i = 0; i < T; i++) {
+		cin >> n;
+		cout << arr[n] << endl;
+	}
 
 
 }
